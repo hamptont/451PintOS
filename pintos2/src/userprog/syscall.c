c59a6f6808e2319fcad6c77e1982309084f043cc
Hampton Terry <hterry@uw.edu>
2012-10-31 14:06:08 -0700

    adding dup2 syscall

--- pintos2/src/userprog/syscall.c
+++ pintos2/src/userprog/syscall.c
@@ -133,7 +133,42 @@ exec (const char *cmd_line)
 static int 
 dup2 (int old_fd, int new_fd)
 {
-  return 0;
+/*
+ * It looks like this implementation should work but I don't see any
+ * tests that test this syscall???
+ *
+ * After this function is called, there are multiple FDs pointing to the 
+ * same file. Not sure if we have to worry about what happens when one
+ * of the FDs call close on it's file, while another FD is still trying
+ * to use the file. 
+ */
+
+  if(old_fd == new_fd)
+  {
+    return -1;
+  }
+ 
+  if(old_fd > 127 || old_fd < 0)
+  {
+    return -1;
+  }
+
+  if(new_fd > 127 || new_fd < 0)
+  {
+    return -1;
+  }
+
+  struct file **fds = thread_current()->fds;
+  struct file *old_file = fds[old_fd];
+
+  if(old_file == NULL)
+  {
+    return -1;
+  }
+
+  fds[new_fd] = old_file;
+
+  return new_fd;
 }
 
 static int 
