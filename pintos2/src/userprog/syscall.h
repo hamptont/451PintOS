afe9595849c848b3474e8383652346c9df36b1d0
Hampton Terry <hterry@uw.edu>
2012-11-03 19:29:41 -0700

    removing struct pipe_buffer

--- pintos2/src/userprog/syscall.h
+++ pintos2/src/userprog/syscall.h
@@ -8,12 +8,13 @@ struct lock filesys_lock;
 void syscall_init (void);
 void exit (int status);
 
-struct pipe_buffer{
-  char buffer[PIPE_BUFFER_SIZE]; //pipe buffer
-  int start; //pointer to next char to be read from buffer
-  int end; //pointer to index to write next char to buffer
-};
+//struct pipe_buffer{
+//  char buffer[PIPE_BUFFER_SIZE]; //pipe buffer
+//  int start; //pointer to next char to be read from buffer
+//  int end; //pointer to index to write next char to buffer
+//};
 
-struct pipe_buffer pipe_buffer_array[MAX_PIPES];
+//array to store all the pipes 
+//struct pipe_buffer pipe_buffer_array[MAX_PIPES];
 
 #endif /* userprog/syscall.h */
