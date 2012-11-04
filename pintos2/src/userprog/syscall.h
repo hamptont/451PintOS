5ddd57630debcf89760e7c7f2d937ad78d3787f2
Hampton Terry <hterry@uw.edu>
2012-11-03 19:34:25 -0700

    fixed pipe buffer struct

--- pintos2/src/userprog/syscall.h
+++ pintos2/src/userprog/syscall.h
@@ -1,20 +1,20 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
-#define PIPE_BUFFER_SIZE 4096 //max number of chars that can be stored in a pipe buffer
+#define PIPE_BUFFER_SIZE 1024 //max number of chars that can be stored in a pipe buffer
 #define MAX_PIPES 128 //max number of pipes that can be open at any given time
 struct lock filesys_lock;
 
 void syscall_init (void);
 void exit (int status);
 
-//struct pipe_buffer{
-//  char buffer[PIPE_BUFFER_SIZE]; //pipe buffer
-//  int start; //pointer to next char to be read from buffer
-//  int end; //pointer to index to write next char to buffer
-//};
+struct pipe_buffer{
+  char buffer[PIPE_BUFFER_SIZE]; //pipe buffer
+  int start; //pointer to next char to be read from buffer
+  int end; //pointer to index to write next char to buffer
+};
 
 //array to store all the pipes 
-//struct pipe_buffer pipe_buffer_array[MAX_PIPES];
+struct pipe_buffer pipe_buffer_array[MAX_PIPES];
 
 #endif /* userprog/syscall.h */
